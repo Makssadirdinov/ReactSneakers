9f87711e7cc582d2a667bcea18786ee8d7ac7ffc
New User <newuser@News-MacBook-Pro.local>
2022-10-14 22:37:19 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,38 @@
+
+import Card from "./components/Card"
+import Drawer from "./components/Drawer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+  <div className="wrapper clear"> 
+
+
+  <Drawer/>
+ <Header/>
+
+  <div className="content p-40">
+    <div className="d-flex align-center justify-between mb-40">
+      <h1>Все кроссовки</h1>
+      <div className="search__block d-flex">
+        <img src="/img/search.svg" alt="search"/>
+        <input placeholder="Поиск..." />
+      </div>
+
+    </div>
+
+    <div className="d-flex">
+
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+
+    </div>
+
+  </div>
+  </div>
+  )
+}
+
+export default App;
