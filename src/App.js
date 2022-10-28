d23fbce840c3b2028e044c98718811ffa35b9985
New User <newuser@News-MacBook-Pro.local>
2022-10-28 01:35:03 +0100

    props, scss modules

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 
-import Card from "./components/Card"
+import Card from "./components/Card/Card"
 import Drawer from "./components/Drawer";
 import Header from "./components/Header";
+
+
+const arr = [
+{title: "Мужские Кроссовки Nike Blazer Mid Suede", price: 12999, imageUrl: "/img/sneakers/1.jpg"},
+{title: "Мужские Кроссовки Nike Air Max 270", price: 15999, imageUrl: "/img/sneakers/2.jpg"},
+{title: "Мужские Кроссовки Nike Blazer Mid Suede", price: 8499, imageUrl: "/img/sneakers/3.jpg"},
+{title: "Кроссовки Puma X Aka Boku Future Rider", price: 8999, imageUrl: "/img/sneakers/4.jpg"},
+]
 
 function App() {
   return (
@@ -22,12 +30,17 @@ function App() {
     </div>
 
     <div className="d-flex">
-
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-
+  
+    
+      {
+      arr.map((el) => (
+        <Card 
+        title={el.title}
+        price={el.price}
+        imageUrl={el.imageUrl} 
+        onClick={() => console.log(el)}
+        />))
+      }
     </div>
 
   </div>
